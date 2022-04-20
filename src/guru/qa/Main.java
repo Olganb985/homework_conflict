9d3940b9c2d8240b79919b2a3a0f86a36ca5bddc
Olganb985 <olganb985@gmail.com>
2022-04-21 06:22:07 +1000

    add new line

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I do my homework");
+        System.out.println("I do my homework very well");
     }
 }
